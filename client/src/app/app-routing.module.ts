2fffbab3cac7141553baa59bda767b016110765c
Natru <swamy422@gmail.com>
2021-03-02 02:37:08 -0800

    Initial commit

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import { ProfileComponent } from "./profile/profile.component";
-import { AuthGuard } from "./auth.guard";
+import { AuthGuard } from "./services/auth.guard";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
