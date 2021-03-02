d5ca0b64c33a6b7b91b5d106626c4bdb99db6c47
Natru Swamy <nswamy@digitalguardian.com>
2021-03-03 03:25:31 +0530

    commit

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 require('./api/models/db');
 require('./api/config/passport');
 
+var socketio = require('socket.io');
+var jwtAuth = require('socketio-jwt-auth')
+var http = require("http"),
+    url = require("url"),
+    mime = require("mime"),
+    fs = require("fs"),
+    SocketIOFileUploadServer = require("./server")
+var io;
+
 const cookieParser = require('cookie-parser');
 const createError = require('http-errors');
 const express = require('express');
@@ -10,11 +19,46 @@ const path = require('path');
 
 const routesApi = require('./api/routes/index');
 
-const app = express();
+// var app = http.createServer(function(req, resp){
+// 	var filename = path.join(__dirname, "public_html", url.parse(req.url).pathname);
+// 	(fs.exists || path.exists)(filename, function(exists){
+// 		if (exists) {
+// 			fs.readFile(filename, function(err, data){
+// 				if (err) {
+// 					// File exists but is not readable (permissions issue?)
+// 					resp.writeHead(500, {
+// 						"Content-Type": "text/plain"
+// 					});
+// 					resp.write("Internal server error: could not read file");
+// 					resp.end();
+// 					return;
+// 				}
+ 
+// 				// File exists and is readable
+// 				var mimetype = mime.lookup(filename);
+// 				resp.writeHead(200, {
+// 					"Content-Type": mimetype
+// 				});
+// 				resp.write(data);
+// 				resp.end();
+// 				return;
+// 			});
+// 		}
+// 	});
+// });
 
+const app = express()
+      .use(SocketIOFileUploadServer.router)
+      .use(express.static(__dirname + "/out"))
+      .use(express.static(__dirname + "/public_html"))
+      // .listen(4567);
+
+const server = app.listen(4200, () => {
+  console.log("Listening on port: " + 4200);
+  });
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -47,6 +91,60 @@ app.use((err, req, res, next) => {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+io = socketio.listen(server);
+
+io.use(jwtAuth.authenticate({
+  secret: 'MY_SECRET',    // required, used to verify the token's signature
+  succeedWithoutToken: true
+}, function(payload, done) {
+
+  console.log("checking connection")
+  // you done callback will not include any payload data now
+  // if no token was supplied
+  if (payload && payload.sub) {
+    User.findOne({id: payload.sub}, function(err, user) {
+      if (err) {
+        // return error
+        return done(err);
+      }
+      if (!user) {
+        // return fail with an error message
+        return done(null, false, 'user does not exist');
+      }
+      // return success with a user info
+      return done(null, user);
+    });
+  } else {
+    return done() // in your connection handler user.logged_in will be false
+  }
+}));
+
+io.sockets.on("connection", function(socket){
+  var siofuServer = new SocketIOFileUploadServer();
+  
+  socket.on('data', (data) => {
+    console.log(data.toString());
+  });
+  
+	siofuServer.on("saved", function(event){
+		console.log(event.file);
+		event.file.clientDetail.base = event.file.base;
+	});
+	siofuServer.on("error", function(data){
+		console.log("Error: "+data.memo);
+		console.log(data.error);
+	});
+	siofuServer.on("start", function(event){
+		if (/\.exe$/.test(event.file.name)) {
+			console.log("Aborting: " + event.file.id);
+			siofuServer.abort(event.file.id, socket);
+		}
+	});
+	siofuServer.dir = "uploads";
+	siofuServer.maxFileSize = 2000;
+	siofuServer.listen(socket);
 });
 
 module.exports = app;
